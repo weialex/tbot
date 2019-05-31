f7a3c5372f91f2f1d4105c63a5ee3d747f96fcbb
Alex Wei <ra157642@fred.lab.ic.unicamp.br>
2019-05-30 21:33:57 -0300

    include a ext file to store sentences for the bot

--- src/core.py
+++ src/core.py
@@ -1,11 +1,11 @@
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 from conf.settings import TELEGRAM_TOKEN, HTTP_CATS_URL
+from conf.texts4messages import *
 
 def start(bot, update):
-    response_message = "Bem vindo ao bot Talklicious."##+'\n'+""
     bot.send_message(
         chat_id = update.message.chat_id,
-        text=response_message
+        text=START_WELCOME
     )
 
 def http_cats(bot, update, args):
@@ -15,10 +15,9 @@ def http_cats(bot, update, args):
     )
 
 def help(bot, update):
-    response_message = "Desculpa não ajudar, nem mesmo eu sei o que estou fazendo."+'\n'+"Mas para te animar, segue esse belo pudim."
     bot.send_message(
         chat_id = update.message.chat_id,
-        text=response_message
+        text=HELP_MESSAGE
     )
     bot.sendPhoto(
         chat_id = update.message.chat_id,
@@ -26,46 +25,89 @@ def help(bot, update):
     )
 
 def about(bot, update):
-    response_message = "Apenas para exercício do aprendizado de chatbot. Feito com a ajuda de: https://github.com/mdcg/http-cats-telegram-bot"
     bot.send_message(
         chat_id = update.message.chat_id,
-        text=response_message
-    )    
- 
+        text=ABOUT_MESSAGE
+    )
+
 def unknown(bot, update):
-    response_message = "Comando inexistente."
     bot.send_message(
         chat_id = update.message.chat_id,
-        text=response_message
+        text=UNKNOWN_MESSAGE
+    )
+    bot.sendAnimation(
+        chat_id = update.message.chat_id,
+        animation="https://i.imgur.com/KxOR0LC.gif"
+    )
+
+def hello(bot, update):
+    bot.send_message(
+        chat_id = update.message.chat_id,
+        text=HELLO_MESSAGE
+    )
+
+def who(bot, update):
+    nome=str(update.message.from_user.first_name)
+
+    bot.send_message(
+        chat_id = update.message.chat_id,
+        text=nome
+    )
+
+def rtext(bot, update):
+
+    bot.send_message(
+
+    )
+
+def raudio(bot, update):
+    bot.send_message(
+        chat_id = update.message.chat_id,
+        text=DONT_ACCEPT_MEDIA
+    )
+    bot.sendAnimation(
+        chat_id = update.message.chat_id,
+        animation="https://media1.tenor.com/images/eb1b27863813653543914d222ceb9cd0/tenor.gif?itemid=5548554"
+    )
+
+def ranimation(bot, update):
+    bot.send_message(
+        chat_id = update.message.chat_id,
+        text=DONT_ACCEPT_MEDIA
+    )
+    bot.sendAnimation(
+        chat_id = update.message.chat_id,
+        animation="https://external-preview.redd.it/QJl4sfn1r2lsRglVwNk9B5UiPuxh4skAU2BRVq4zDsQ.gif?format=mp4&s=e0aaa48d55c67053be16639e131a0017944fecc5"
     )
 
 def main():
     updater = Updater(token = TELEGRAM_TOKEN)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(
-        CommandHandler('start', start)
-    )
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('http', http_cats, pass_args=True))
 
-    dispatcher.add_handler(
-        CommandHandler('http', http_cats, pass_args=True)
-    ) #arg eh o argumento que vem junto do comando no telegram /command arg
+    dispatcher.add_handler(CommandHandler('who', who))
 
-    dispatcher.add_handler(
-        CommandHandler('help', help)
-    )
+    dispatcher.add_handler(CommandHandler('help', help))
+    dispatcher.add_handler(CommandHandler('about', about))
 
-    dispatcher.add_handler(
-        CommandHandler('about', about)
-    )
 
-    dispatcher.add_handler(
-        MessageHandler(Filters.command, unknown)
-    ) #no caso do cliente enviar algo diferente de um dos comandos
-      #atribuidos acima, cai em unknown()
+    #dispatcher.add_handler(MessageHandler('oi', hello))
 
-    updater.start_polling() #busca atualizacoes do telegram ao chatbot
-    updater.idle() #bloqueia ate receber um sinal e pare o updater
+    #command don't expected
+    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+
+    dispatcher.add_handler(MessageHandler(Filters.text, rtext))
+
+    dispatcher.add_handler(MessageHandler(Filters.audio | Filters.voice, raudio))
+    dispatcher.add_handler(MessageHandler(Filters.animation | Filters.video, ranimation))
+
+    #start the bot
+    updater.start_polling()
+
+    # works until ctrl+c
+    updater.idle()
 
 if __name__ == '__main__':
     print("press CTRL + C to cancel.")
